797a5fec69f8f8605e7f72d1b3302805ddfff60d
trongnguyen24 <trongnguyen24@gmail.com>
2026-06-01 11:21:13 +0700

    feat: add browser extension icons and configure manifest icon support

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -15,6 +15,24 @@ export default defineConfig({
       'https://*.workers.dev/*',
       'https://*.pages.dev/*'
     ],
+    icons: {
+      "16": "favicon.png",
+      "32": "favicon.png",
+      "48": "favicon.png",
+      "96": "icon-192.png",
+      "128": "icon-192.png",
+      "256": "icon-256.png",
+      "512": "icon-512.png"
+    },
+    action: {
+      default_icon: {
+        "16": "favicon.png",
+        "32": "favicon.png",
+        "48": "favicon.png",
+        "96": "icon-192.png",
+        "128": "icon-192.png"
+      }
+    }
   },
   vite: () => ({
     plugins: [tailwindcss()],
